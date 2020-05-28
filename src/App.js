c7a3a047a0770e92b31a3d1e5cf1d351f919c7b4
Ricardo Gerbaudo <ricardogerbaudo@MacBook.local>
2020-05-28 05:28:00 -0300

    Convert original static files into React structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
+<div class="boards">
+        <div class="board">
+            <h3>To do</h3>
+            <div class="dropzone">
+                <div class="card" draggable="true">
+                    <div class="status red"></div>
+                    <div class="content">Record videos</div>
+                </div>
+            </div>
+        </div>
+        <div class="board">
+            <h3>In progress</h3>
+            <div class="dropzone">
+                <div class="card" draggable="true">
+                    <div class="status green"></div>
+                    <div class="content">Forum</div>
+                </div>
+            </div>
+        </div>
+        <div class="board">
+            <h3>Done</h3>
+            <div class="dropzone">
+                <div class="card" draggable="true">
+                    <div class="status blue"></div>
+                    <div class="content">Next Level Week</div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
